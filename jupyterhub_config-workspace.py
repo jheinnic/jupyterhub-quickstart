b956967d3fa0b857864d4020a496816fb1e19c1f
John Heinnickel <jheinnic@gmail.com>
2020-08-30 22:04:59 -0700

    Attempting to get template expressions working again
    Simplification.  Fewer punctuation chars in the patterns for OAuth and
    Datbase passwords.  Use hex instead of base64 for capturing the
    encryption key.

--- jupyterhub_config-workspace.py
+++ jupyterhub_config-workspace.py
@@ -15,7 +15,8 @@ c.OpenShiftOAuthenticator.client_id = client_id
 c.OpenShiftOAuthenticator.client_secret = client_secret
 c.Authenticator.enable_auth_state = True
 
-c.CryptKeeper.keys = [ os.environ['JUPYTERHUB_ENCRYPTION_KEY'] ]
+hex_key = os.environ['JUPYTERHUB_ENCRYPTION_KEY']
+c.CryptKeeper.keys = b64encode(bytes.fromhex(hex_key)).decode()
 
 c.OpenShiftOAuthenticator.openshift_auth_api_url = \
     os.environ['OPENSHIFT_AUTH_API_URL']
